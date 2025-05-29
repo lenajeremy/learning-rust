2870e8650ef8a0f48b6b8f055ca8db3562a5ee31
lenjeremy <jeremiahlena13@gmail.com>
2025-05-29 09:19:45 +0100

    completed chapter 2

---
+++ chapter2/guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number");
+    let mut guesses: u8 = 0;
+    // generate random number
+    let secret_number = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Enter an integer");
+        let mut guess = String::new();
+        guesses += 1;
+
+        // read user input
+        io::stdin().read_line(&mut guess).expect("invalid input");
+
+        // parse the integer from the input
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // compare to the generated number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess is lower than the answer"),
+            Ordering::Greater => println!("Your guess is greater than the answer"),
+            Ordering::Equal => {
+                println!("Yayyyy! You got the answer correctly");
+                println!("You took {} guesses", guesses);
+                break;
+            }
+        }
+    }
+}
+
